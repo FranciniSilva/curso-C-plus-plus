bcc852fe48d2f4e0cd9144d2c9d4076f42195ea3
FranciniSilva <franciniassis89@hotmail.com>
2016-09-18 16:54:19 -0300

    Tomada de decisa I

---
+++ tomadaDeDecisaoI.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int i =  5, i2 = 10;
+
+	cout << "Se o valor e i for igual ao valor de i2, a frase a seguir será impressa" << endl;
+
+	if (i == i2)
+	{
+		cout << "A variável i é igual a variável i2" <<	endl;
+	}
+
+	cout << endl;
+
+	if (i != i2)
+	{
+		cout << "i é diferente de i2" << endl;
+	}
+
+	 return 0;
+}
