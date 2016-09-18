c9306bb7bc02f511378784a8c6580357e77090a4
FranciniSilva <franciniassis89@hotmail.com>
2016-09-18 12:27:30 -0300

    Utilizando comentário em meio ao código

--- olaMundo.cpp
+++ olaMundo.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
+/*
+* Comentário de mais linhas
+* Bloco de comentários
+*/
+
 int main() {
 
+	//A mensagem que está entre aspas é o que será impresso na tela
 	cout << "Olá, Mundo!" << endl;
 	return 0;
 }
