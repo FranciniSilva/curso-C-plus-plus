90de19252fdddecca00bfbee142a697a334a894f
FranciniSilva <franciniassis89@hotmail.com>
2016-09-18 18:22:15 -0300

    Operadores de Atribuição

---
+++ operadoresDeAtribuicao.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int x = 20;
+	x = x * 10;
+	x *= 10;
+	x -= 10;
+	x += 10;
+	x /= 10;
+	x %= 10;
+
+	cout <<  << endl;
+
+	return 0;
+}
