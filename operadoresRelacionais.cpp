b96e5df9baef59824d06832d099694ee3ac09d7c
FranciniSilva <franciniassis89@hotmail.com>
2016-09-18 16:33:04 -0300

    Operadores Relacionais

---
+++ operadoresRelacionais.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	bool b = true;
+	bool b2 = (1 == 1);
+
+	cout << "O valor contido em 'b' é igual a: " << b << endl;
+	cout << "O valor contido em 'b2' é igual a: " << b2 << endl;
+
+	int c = 0;
+	int c2 = 1;
+
+	cout << "O valor e: " << (c2 == true) << endl;
+	cout << "O valor e: " << (c == true) << endl;
+
+
+	return 0;
+}
