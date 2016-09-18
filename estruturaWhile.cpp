1624934b185c3eb3b2a7c5c333099ce7231158cd
FranciniSilva <franciniassis89@hotmail.com>
2016-09-18 19:33:34 -0300

    Estrutura While

---
+++ estruturaWhile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int num = 0;
+
+	while(num < 100){
+		cout << num << endl;
+		num++;
+	}
+
+	int num2 = 0;
+
+	while(num2 > 0){
+		cout << num2 << endl;
+		num--;
+	}
+
+	return 0;
+}
