f4867559b9d65021066ee026cca2fc25cb3094c3
FranciniSilva <franciniassis89@hotmail.com>
2016-09-18 12:42:51 -0300

    Manipulando variáveis

---
+++ tiposPrimitivos.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Tipos primitivos" << endl;
+	cout << endl;
+	int varInt = 100;
+	char c = 'r';
+	double pFlutuante= 5.99;
+
+	cout << "O valor da variável varInt é: " << varInt << endl;
+	cout << "O valor da variável c é: " << c << endl;
+	cout << "O valor da variável pFlutuante é: " << pFlutuante << endl;
+	cout << endl;
+	cout << "Memória da variável varInt: " << sizeof(varInt) << " bytes" << endl;
+	cout << "Memória da  variável c: " << sizeof(c) << " bytes" << endl;
+	cout << "Memória da variável pFlutuante: " << sizeof(pFlutuante) << " bytes" << endl; 
+
+	return 0;
+}
