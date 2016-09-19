1f7ce149da1b7b69684f21a2b0586652c48d5a6f
FranciniSilva <franciniassis89@hotmail.com>
2016-09-19 15:26:23 -0300

    Arrays no C++ II

--- arrays.cpp
+++ arrays.cpp
@@ -4,12 +4,21 @@ using namespace std;
 
 int main() {
 
-	int nums[5];
+	int nums[10];
 
-	nums[0] = 10;
+	nums[0] = 100;
+	nums[1] = 99;
+	nums[2] = 50;
+	nums[3] = 25;
+	nums[4] = 4;
+	nums[5] = 5;
+	nums[6] = 60;
+	nums[7] = 0;
+	nums[8] = -1;
+	nums[9] = 9;
 
-	double valores[100];
-	valores[50] = 5.55;
+	cout << nums[5] + nums[9] << endl;
+	cout << nums[0] << endl;
 
 	return 0;
 }
