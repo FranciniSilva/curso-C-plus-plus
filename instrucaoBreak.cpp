25c2afae74c9ccb560310259b8169372adae996f
FranciniSilva <franciniassis89@hotmail.com>
2016-09-19 13:40:48 -0300

    INstrução continue

--- instrucaoBreak.cpp
+++ instrucaoBreak.cpp
@@ -8,10 +8,17 @@ int main() {
 
 	while(true){
 		n++;
-		cout << n << endl;
+		
 
-		if (n >= 10)
+		if (n >= 10) {
 			break;
+		}else{
+			if(n == 4 or n == 6){
+				continue; 
+			}
+		}
+
+	cout << n << endl;
 	}
 
 	return 0;
